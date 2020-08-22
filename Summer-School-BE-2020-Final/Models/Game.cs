ccd312e0817db6efa172b38b9538e1a4e890c020
daniil789 <pechenkin031199@mail.ru>
2020-08-22 17:27:07 +0500

    Add project files.

---
+++ Summer-School-BE-2020-Final/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Summer_School_BE_2020_Final.Models
+{
+    public class Game
+    {
+        private string Name {get;set;}
+        private string Key { get; set; }
+
+    }
+}
